048ce98311201802b2f57ffdeea08f6f28e1712f
shollercoaster <saumyachat03@gmail.com>
2021-06-26 15:46:43 +0530

    initial commit

---
+++ madlib.py
@@ -0,0 +1,26 @@
+import re
+def madlib():
+    fresh=open("fresh.txt", 'w+')
+    story=open("story.txt", 'r')
+    with open("write.txt", 'r+') as output:
+        read=story.readlines()
+        write=output.readlines()
+#        print(read)
+#        print(write)
+        for iter in range(0,11):
+                print(read[iter], '\n')
+                choice= input("enter choice:")
+                line=write[iter]
+                linenew=re.sub("_", choice, line)
+                fresh.write(linenew)
+        print("the final story is:")
+        fresh.seek(0)
+        final=fresh.read()
+        print(final)
+
+#if __name__== "__main__":
+madlib()
+fresh=open("fresh.txt", 'r+')
+fresh.truncate()
+fresh.close()
+    
